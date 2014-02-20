cc6d953c17a41ad0774f95c864acfef81af43b5e
Anthony Roberts <nns.aroberts@gmail.com>
2014-02-20 13:57:03 -0600

    django 1.5 url import, not backwards compatible

--- django_ses/urls.py
+++ django_ses/urls.py
@@ -1,7 +1,4 @@
-try:
-    from django.conf.urls import patterns, url
-except ImportError: # django < 1.4
-    from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('django_ses.views',
     url(r'^$', 'dashboard', name='django_ses_stats'),
